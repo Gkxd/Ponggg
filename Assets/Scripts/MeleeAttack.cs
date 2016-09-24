a167c4dae7cae80840511489c65982058fa9c069
Gkxd <gkxd@users.noreply.github.com>
2016-09-24 17:37:01 -0400

    Added pong ball

---
+++ Assets/Scripts/MeleeAttack.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeAttack : MonoBehaviour
+{
+    [Range(30, 120)]
+    public float angle;
+
+    [Range(1, 3)]
+    public float range;
+
+    private float currentAngle;
+
+    private MeshFilter meshFilter;
+    private Mesh mesh;
+
+    private Vector3 lastPosition;
+
+    void Start()
+    {
+        currentAngle = -0.5f * angle;
+        meshFilter = GetComponent<MeshFilter>();
+
+        mesh = new Mesh();
+        meshFilter.mesh = mesh;
+    }
+
+    void FixedUpdate()
+    {
+        currentAngle = Mathf.Lerp(0.5f * angle, currentAngle, 50);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PongBall.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PongBall : MonoBehaviour
+{
+
+    public float speed;
+
+    private new Rigidbody rigidbody;
+
+    private Vector3 direction;
+
+    private bool hitLeft, hitTop, hitRight, hitBottom;
+
+    void Start()
+    {
+        direction = Random.insideUnitCircle.normalized;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.velocity = direction * speed;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Reflect(collision.contacts[0].normal);
+    }
+
+    public void Reflect(Vector3 normal)
+    {
+        direction = Vector3.Reflect(direction, normal);
+    }
+}
