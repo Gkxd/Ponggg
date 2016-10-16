10c4e227dfb93a93e1a0450b2fba90e0dc90c033
Gkxd <gkxd@users.noreply.github.com>
2016-10-16 12:38:08 -0400

    Updating bullets

---
+++ Assets/Scripts/BulletBehaviours/SizeChangingBullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UsefulThings;
+
+public class SizeChangingBullet : MonoBehaviour
+{
+    public Curve sizeChangeCurve;
+
+    private float startTime;
+    private Vector3 startSize;
+
+    void Start()
+    {
+        startTime = Time.time;
+        startSize = transform.localScale;
+        transform.localScale = startSize * sizeChangeCurve.Evaluate(0);
+    }
+
+    void Update()
+    {
+        transform.localScale = startSize * sizeChangeCurve.Evaluate(Time.time - startTime);
+    }
+}
