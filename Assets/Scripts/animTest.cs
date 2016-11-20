0fb11dc8321388420e2220f97c6ad7a41603c6c4
Ian Griswold <igriswol@andrew.cmu.edu>
2016-11-20 12:56:01 -0500

    Animation test, confirmed to work, example in animtest

---
+++ Assets/Scripts/animTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animTest : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator.SetBool ("run1", Input.GetKey (KeyCode.C));
+		animator.SetBool ("run2", Input.GetKey (KeyCode.V));
+		animator.SetBool ("run3", Input.GetKey (KeyCode.B));
+	}
+}
