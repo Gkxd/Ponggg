020f9b8fd184f6db2b4706ed4349a2a5e06227e2
Ian Griswold <igriswol@andrew.cmu.edu>
2016-11-20 13:39:53 -0500

    Added three test composite attacks

---
+++ Assets/TwoAttacks.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TwoAttacks : MonoBehaviour {
+	
+	public GameObject attack1;
+	public GameObject attack2;
+	public float verticalSeparation;
+
+	void Start () {
+		Vector3 pos1 = new Vector3 (transform.position.x, transform.position.y + verticalSeparation, transform.position.z);
+		Vector3 pos2 = new Vector3 (transform.position.x, transform.position.y - verticalSeparation, transform.position.z);
+
+		GameObject mov1 = (GameObject)Instantiate (attack1, pos1, Quaternion.FromToRotation(Vector3.right,Vector3.right));
+		GameObject mov2 = (GameObject)Instantiate (attack2, pos2, Quaternion.FromToRotation(Vector3.right,Vector3.right));
+	}
+
+	void Update () {
+		
+	}
+}
